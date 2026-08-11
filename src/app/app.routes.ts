08133e5d5650307b277234566058f35fc4670237
Carlos Alberto <cajunior2612@gmail.com>
2026-08-11 10:58:16 -0300

    login feito

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/auth.guard';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+    {
+        path: 'home',
+        loadComponent: () => import('./features/home/home/home').then(m => m.Home),
+    },
+    {
+        path: 'carrinho',
+        canActivate: [authGuard],
+        loadComponent: () => import('./features/carrinho/carrinho/carrinho').then(m => m.Carrinho),
+    },
+    {
+        path: 'produtos',
+        loadComponent: () => import('./features/produtos/lista-produtos/lista-produtos').then(m => m.ListaProdutos),
+    },
+    {
+        path: '**',
+        redirectTo: 'home',
+    }
+
+];
+
+
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,16 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Produto } from './features/produtos/produto/produto';
-import { ListaProdutos} from './features/produtos/lista-produtos/lista-produtos';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import {usuarioLogado, login, logout} from './core/auth';
 
 @Component({
-  selector: 'app-root',
-  imports: [ ListaProdutos ],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+selector: 'app-root',
+imports: [RouterOutlet, RouterLink],
+templateUrl: './app.html',
+styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('meu-primeiro-app');
+    usuarioLogado = usuarioLogado;
+    login = login;
+    logout = logout;
 }
+
